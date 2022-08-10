ddd8bc0bc672175b032e1cdf4c346cb5f04a3928
Frank Wray <idmfrank@gmail.com>
2022-08-10 18:06:27 +0000

    Changing project from gitpod to replit.com

--- src/app.py
+++ src/app.py
@@ -1,18 +1,46 @@
-from flask import Flask, jsonify
+# using flask_restful
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+  
+# creating the flask app
 app = Flask(__name__)
-
-@app.route('/hello', methods=['GET'])
-def hello():
-    # suppose you have your data in the variable name_data
-    name_data = { "firstName": "PinKanki", "lastName": "Flask" }
-
-    # you can convert that variable into a json string like this
-    json_text = jsonify(name_data)
-
-    # and then you can return it to the front end in the response body like this
-    return json_text
-
-
-# These two lines should always be at the end of your app.py file.
+# creating an API object
+api = Api(app)
+  
+# making a class for a particular resource
+# the get, post methods correspond to get and post requests
+# they are automatically mapped by flask_restful.
+# other methods include put, delete, etc.
+class Hello(Resource):
+  
+    # corresponds to the GET request.
+    # this function is called whenever there
+    # is a GET request for this resource
+    def get(self):
+  
+        return jsonify({'message': 'hello world'})
+  
+    # Corresponds to POST request
+    def post(self):
+          
+        data = request.get_json()     # status code
+        return jsonify({'data': data}), 201
+  
+  
+# another resource to calculate the square of a number
+class Square(Resource):
+  
+    def get(self, num):
+  
+        return jsonify({'square': num**2})
+  
+  
+# adding the defined resources along with their corresponding urls
+api.add_resource(Hello, '/')
+api.add_resource(Square, '/square/<int:num>')
+  
+  
+# driver function
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+  
+    app.run(debug = True)
